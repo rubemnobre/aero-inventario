f84b29ec75d40e5863a202cd4a76cccc46f6bfb5
rubemnobre <rubemjrx@hotmail.com>
2021-02-16 16:11:45 -0300

    separar função de tabela

--- main.py
+++ main.py
@@ -1,42 +1,10 @@
 import mysql.connector
 from mysql.connector import Error
+from table import table
 
 ## Coisas a adicionar:
 # Remover Itens
 # Quantidades
-
-def table(names, lista):
-    ncols = len(lista[0])
-    nrows = len(lista)
-    mat = []
-    for i in range(ncols):
-        mat.append([])
-        for j in range(nrows):
-            mat[i].append(str(lista[j][i]))
-    lens = []
-    for i in range(ncols):
-        lens.append(max(len(max(mat[i], key=len)), len(names[i])))
-
-    for i in range(ncols):
-        print('+' + (lens[i]+2)*'-', end='')
-    print('+')
-
-    for i in range(ncols):
-        print('| %s ' % (names[i] + (lens[i]-len(names[i]))*' '), end='')
-    print('|')
-
-    for i in range(ncols):
-        print('+' + (lens[i]+2)*'-', end='')
-    print('+')
-
-    for i in range(nrows):
-        for j in range(ncols):
-            print('| %s ' % (mat[j][i] + (lens[j]-len(mat[j][i]))*' '), end='')
-        print('|')
-
-    for i in range(ncols):
-        print('+' + (lens[i]+2)*'-', end='')
-    print('+')
 
 server = mysql.connector.connect(
     host = 'sql10.freemysqlhosting.net',
